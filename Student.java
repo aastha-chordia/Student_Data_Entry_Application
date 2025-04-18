ca6a92b02dd9f2dcaea60795267a7e49b159a194
Aastha Chordia <aasthachordia595@gmail.com>
2025-04-19 02:25:50 +0530

    Update Student.java
    public class

--- Student.java
+++ Student.java
@@ -3,3 +3,9 @@ public class Student {
     private String name;
     private int age;
     private String prn;
+
+    public Student(String name, int age, String prn) {
+        this.name = name;
+        this.age = age;
+        this.prn = prn;
+    }
