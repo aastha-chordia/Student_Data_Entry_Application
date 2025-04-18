5d3b00b72205e1dba88b8bbe939182588ba6f8c2
Aastha Chordia <aasthachordia595@gmail.com>
2025-04-19 01:33:07 +0530

    1st Print

--- Main.java
+++ Main.java
@@ -7,4 +7,9 @@ AIML A1
 import java.util.Scanner;
 
 public class Main {
-   
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        StudentManager manager = new StudentManager();
+        
+        System.out.println("Student Data Entry System");
+       
