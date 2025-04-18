a9743bb008b8ef8512903d9bb58a7e7f3e78fac7
Aastha Chordia <aasthachordia595@gmail.com>
2025-04-19 02:35:06 +0530

    for loop to traverse

--- StudentDatabase.java
+++ StudentDatabase.java
@@ -5,3 +5,5 @@ public class StudentDatabase {
     private List<Student> studentList = new ArrayList<>();
     
     public void addStudent(Student student) throws DuplicatePRNException {
+        for (Student s : studentList) {
+            
