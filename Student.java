a4ad53f72eaa02d6628098bf8170c2690f17602b
Aastha Chordia <aasthachordia595@gmail.com>
2025-04-19 02:26:38 +0530

    getAge

--- Student.java
+++ Student.java
@@ -12,3 +12,6 @@ public class Student {
 
     public String getName() { return name; }
     public void setName(String name) { this.name = name; }
+    
+    public int getAge() { return age; }
+    public void setAge(int age) { this.age = age; }
