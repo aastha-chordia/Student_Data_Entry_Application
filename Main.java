8ff8c378d0eb99a536548396c809707b981cedba
Aastha Chordia <aasthachordia595@gmail.com>
2025-04-19 01:33:53 +0530

    menu creation

--- Main.java
+++ Main.java
@@ -14,4 +14,12 @@ public class Main {
         System.out.println("Student Data Entry System");
         
         while (true) {
+            System.out.println("\nMenu:");
+            System.out.println("1. Add Student");
+            System.out.println("2. View All Students");
+            System.out.println("3. Search Student by PRN");
+            System.out.println("4. Update Student");
+            System.out.println("5. Delete Student");
+            System.out.println("6. Exit");
+            
            
