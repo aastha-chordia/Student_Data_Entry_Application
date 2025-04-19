44d3e2bac3db82530bf1ffe0070eac5eae908788
Aastha Chordia <aasthachordia595@gmail.com>
2025-04-19 14:59:37 +0530

    final changes

--- Student.java
+++ Student.java
@@ -1,25 +1,66 @@
 // Student.java
-public class Student {
-    private String name;
-    private int age;
-    private String prn;
+// Student.java
 
-    public Student(String name, int age, String prn) {
-        this.name = name;
-        this.age = age;
-        this.prn = prn;
-    }
+class Student {
+  private String name;
+  private long prn;
+  private String branch;
+  private String batch;
+  private double cgpa;
 
-    public String getName() { return name; }
-    public void setName(String name) { this.name = name; }
-    
-    public int getAge() { return age; }
-    public void setAge(int age) { this.age = age; }
-    
-    public String getPrn() { return prn; }
-    public void setPrn(String prn) { this.prn = prn; }
-    
-    public String toString() {
-        return "Name: " + name + ", Age: " + age + ", PRN: " + prn;
-    }
+  // Parameterized Constructor
+  public Student(String name, long prn, String branch, String batch, double cgpa) {
+    this.name = name;
+    this.prn = prn;
+    this.branch = branch;
+    this.batch = batch;
+    this.cgpa = cgpa;
+  }
+
+  // Getter-Setter Methods for private members
+  public String getName() {
+    return name;
+  }
+
+  public void setName(String name) {
+    this.name = name;
+  }
+
+  public long getPRN() {
+    return prn;
+  }
+
+  public void setPRN(long prn) {
+    this.prn = prn;
+  }
+
+  public String getBranch() {
+    return branch;
+  }
+
+  public void setBranch(String branch) {
+    this.branch = branch;
+  }
+
+  public String getBatch() {
+    return batch;
+  }
+
+  public void setBatch(String batch) {
+    this.batch = batch;
+  }
+
+  public double getCGPA() {
+    return cgpa;
+  }
+
+  public void setCGPA(double cgpa) {
+    this.cgpa = cgpa;
+  }
+
+  // Method to display students
+  public void display() {
+    System.out.println(
+        "Student Name: " + name + "\nPRN: " + prn + "\nBranch: " + branch + "\nBatch: " + batch + "\nCGPA: " + cgpa);
+  }
 }
