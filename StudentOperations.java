36a592db4eb88226e47110fdadf9e08dd9be309e
Aastha Chordia <aasthachordia595@gmail.com>
2025-04-19 15:01:44 +0530

    updating the list

--- StudentOperations.java
+++ StudentOperations.java
@@ -7,5 +7,7 @@ import exceptions.InvalidPositionException;
 import exceptions.StudentNotFoundException;
 
 class StudentOperations {
-    ArrayList<Student> students; // array list of data type Student (object) which is a combination of String and
-   
+    ArrayList<Student> students; // array list of data type Student (object) which is a combination of String and int
+   public StudentOperations() {
+        this.students = new ArrayList<>();
+    }
