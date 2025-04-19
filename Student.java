00afe1d7a765774cf881605811b3666defac58fd
Aastha Chordia <aasthachordia595@gmail.com>
2025-04-19 15:43:18 +0530

    errors fiing

--- Student.java
+++ Student.java
@@ -1,65 +1,68 @@
 // Student.java
 
-class Student {
-  private String name;
-  private long prn;
-  private String branch;
-  private String batch;
-  private double cgpa;
+// Student.java
+public class Student {
+    private String name;
+    private long prn;
+    private String branch;
+    private String batch;
+    private double cgpa;
 
-  // Parameterized Constructor
-  public Student(String name, long prn, String branch, String batch, double cgpa) {
-    this.name = name;
-    this.prn = prn;
-    this.branch = branch;
-    this.batch = batch;
-    this.cgpa = cgpa;
-  }
+    public Student(String name, long prn, String branch, String batch, double cgpa) {
+        this.name = name;
+        this.prn = prn;
+        this.branch = branch;
+        this.batch = batch;
+        this.cgpa = cgpa;
+    }
 
-  // Getter-Setter Methods for private members
-  public String getName() {
-    return name;
-  }
+    public String getName() {
+        return name;
+    }
 
-  public void setName(String name) {
-    this.name = name;
-  }
+    public void setName(String name) {
+        this.name = name;
+    }
 
-  public long getPRN() {
-    return prn;
-  }
+    public long getPRN() {
+        return prn;
+    }
 
-  public void setPRN(long prn) {
-    this.prn = prn;
-  }
+    public void setPRN(long prn) {
+        this.prn = prn;
+    }
 
-  public String getBranch() {
-    return branch;
-  }
+    public String getBranch() {
+        return branch;
+    }
 
-  public void setBranch(String branch) {
-    this.branch = branch;
-  }
+    public void setBranch(String branch) {
+        this.branch = branch;
+    }
 
-  public String getBatch() {
-    return batch;
-  }
+    public String getBatch() {
+        return batch;
+    }
 
-  public void setBatch(String batch) {
-    this.batch = batch;
-  }
+    public void setBatch(String batch) {
+        this.batch = batch;
+    }
 
-  public double getCGPA() {
-    return cgpa;
-  }
+    public double getCGPA() {
+        return cgpa;
+    }
 
-  public void setCGPA(double cgpa) {
-    this.cgpa = cgpa;
-  }
+    public void setCGPA(double cgpa) {
+        this.cgpa = cgpa;
+    }
 
-  // Method to display students
-  public void display() {
-    System.out.println(
-        "Student Name: " + name + "\nPRN: " + prn + "\nBranch: " + branch + "\nBatch: " + batch + "\nCGPA: " + cgpa);
-  }
+    public void display() {
+        System.out.println(
+            "Student Name: " + name + 
+            "\nPRN: " + prn + 
+            "\nBranch: " + branch + 
+            "\nBatch: " + batch + 
+            "\nCGPA: " + cgpa
+        );
+    }
 }
